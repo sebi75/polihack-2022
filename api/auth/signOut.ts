8c9806adcc6fa3e22c1e1eefb98d74bfa61add0e
Sebastian <xsebyyy@gmail.com>
2022-12-09 19:49:52 +0200

    signout function

---
+++ api/auth/signOut.ts
@@ -0,0 +1,16 @@
+import { auth } from "../../config";
+import { signOut as signOutFirebase } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const signOut = async () => {
+  try {
+    await signOutFirebase(auth);
+    const signedInUser: any = await AsyncStorage.getItem("loggedInUser");
+    const parsedUser = JSON.parse(signedInUser);
+    await AsyncStorage.removeItem(parsedUser.uid);
+    await AsyncStorage.removeItem("loggedInUser");
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
