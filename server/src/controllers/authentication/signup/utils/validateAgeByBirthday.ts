f8df1b10438801bfca94dbdb4cdfac4d0c558dec
Sebastian Semeniuc <xsebyyy@gmail.com>
2022-12-24 16:48:08 +0200

    changed the date format in fixed type of string

--- server/src/controllers/authentication/signup/utils/validateAgeByBirthday.ts
+++ server/src/controllers/authentication/signup/utils/validateAgeByBirthday.ts
@@ -1,11 +1,12 @@
-//validate the age to be over 18 by birthdate
-export const isValidAge = (birthday: Date) => {
-    const today = new Date();
-    const birthDate = new Date(birthday);
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age >= 18;
-}
+export const isValidAge = (birthday: string) => {
+  // birthday is a string in the format DD-MM-YYYY
+  //validate the age to be over 18 by birthdate
+  const today = new Date();
+  const birthDate = new Date(birthday);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age >= 18;
+};
--- server/src/models/users/User.ts
+++ server/src/models/users/User.ts
@@ -1,14 +1,21 @@
 export type UserResultType = {
-    email: string;
-    about: string;
-    userId: string;
-    birthday: Date;
-    fullName: string;
-    createdAt: number;
-    updatedAt: number;
-    isVerified: boolean;
-    profilePicture: string;
-}
+  email: string;
+  about: string;
+  userId: string;
+  birthday: string;
+  fullName: string;
+  createdAt: number;
+  updatedAt: number;
+  isVerified: boolean;
+  profilePicture: string;
+};
 
-type ExcludedKeysUnion = "userId" | "createdAt" | "updatedAt" | "isVerified" | "profilePicture" | 'fullName' | 'about'
-export type UserCreate = Omit<UserResultType, ExcludedKeysUnion> & { password: string }
+type ExcludedKeysUnion =
+  | 'userId'
+  | 'createdAt'
+  | 'updatedAt'
+  | 'isVerified'
+  | 'profilePicture'
+  | 'fullName'
+  | 'about';
+export type UserCreate = Omit<UserResultType, ExcludedKeysUnion> & { password: string };
